e46ad03dcafc4723c415619b28f6dadf334222c2
taratao <huynh3326@gmail.com>
2020-04-15 17:36:45 +0700

    update files

--- menu.php
+++ menu.php
@@ -1,3 +1,3 @@
 <?php
-echo '<a href="./index.php" class="nav">Home</a><a href="./add.php" class="nav">Add user</a>'
+echo '<a href="./index.php" class="nav">Home</a><a href="./add.php" class="nav">Add user</a><a href="./list.php" class="nav">List user</a>'
 ?>
