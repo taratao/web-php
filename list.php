9c7e5141825179f6daad59107ceb0feb3c52b056
taratao <huynh3326@gmail.com>
2020-04-15 20:12:19 +0700

    update list.php file

--- list.php
+++ list.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List user</title>
 </head>
@@ -9,7 +10,27 @@
 <div class='container'>
     <div class='menu'><?php include 'menu.php'?></div>
     <div class='content'>
+            <?php 
+              $connect = new mysqli('localhost', 'admin', '1234', 'QL_user');
+              if($connect->connect_error){
+                  die("Connection failed: " . $conn->connect_error);
+              }
 
+              $sql = 'SELECT * FROM user';
+              $result = $connect->query($sql);
+
+              if($result->num_rows > 0){
+                  echo '<table>';
+                  echo '<tr><th>user name<th><th>Password</th></tr>';
+                  while($result=$result->fetch_assoc()){
+                      echo '<tr><td>'. $result['username'] . '</td><td>'. $result['password'] . '</td></tr>';
+                  }
+                  echo '</table>';
+              }
+              else{
+                  echo '<span class="msg">Empty</span>';
+              }
+              ?>
     </div>
 </body>
 </html>
