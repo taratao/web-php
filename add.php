797bf23e6fccb6dd5a3d0eced2b7da88036f1c85
taratao <huynh3326@gmail.com>
2020-04-15 17:59:02 +0700

    update files

--- add.php
+++ add.php
@@ -1,2 +1,30 @@
-add user name
-add user name2
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add user</title>
+</head>
+<body>
+<div class='container'>
+    <div class='menu'><?php include 'menu.php'?></div>
+    <div class='form'>
+        <form  action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="GET">
+           <table>
+               <tr>
+                   <td>Tên tài khoản</td>
+                   <td><input type='input' name='uname' value=''></td>
+               </tr>
+               <tr>
+                   <td>Mật khẩu</td>
+                   <td><input type='input' name='passwd' value=''></td>
+               </tr>
+               <tr>
+                   <td></td>
+                   <td><input type='submit' value='Submit'></td>
+               </tr>
+           </table>
+        </form>
+    </div>
+</body>
+</html>
